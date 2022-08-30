081cd29125444a56296e2a252869e7677015b883
yanagitani <takanori.yanagitani@gmail.com>
2022-08-30 10:33:50 +0900

    test: more coverage

--- pkg/postgres/pgx/pgx2kv_test.go
+++ pkg/postgres/pgx/pgx2kv_test.go
@@ -1,8 +1,42 @@
 package pgx2kv
 
 import (
+	"context"
+	"os"
 	"testing"
+
+	"github.com/jackc/pgx/v4/pgxpool"
+
+	s2k "github.com/takanoriyanagitani/go-sql2keyval"
 )
 
-func TestPgxBulkSetNew(t *testing.T) {
+func TestAll(t *testing.T) {
+	t.Parallel()
+
+	pgx_dbname := os.Getenv("ITEST_SQL2KEYVAL_PGX_DBNAME")
+	if len(pgx_dbname) < 1 {
+		t.Skip("skipping pgx test...")
+	}
+
+	p, e := pgxpool.Connect(context.Background(), "dbname="+pgx_dbname)
+	if nil != e {
+		t.Errorf("Unable to connect to test db: %v", e)
+	}
+
+	t.Run("PgxBulkSetNew", func(t *testing.T) {
+		t.Parallel()
+		var sm s2k.SetMany = PgxBulkSetNew(p)
+
+		t.Run("empty", func(t *testing.T) {
+			t.Parallel()
+			e := sm(context.Background(), "", nil)
+			if nil != e {
+				t.Errorf("Should be nop: %v", e)
+			}
+		})
+	})
+
+	t.Cleanup(func() {
+		p.Close()
+	})
 }
